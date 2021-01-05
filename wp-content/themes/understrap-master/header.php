41aaeb0a6c6df47b7973c3a758301e23dbdefd68
draghicimariangeany <draghicimariangeany@gmail.com>
2021-01-04 19:42:31 -0800

    modificare header: adaugare imagine si logo Signed-off-by: draghicimariangeany <draghicimariangeany@gmail.com>

--- wp-content/themes/understrap-master/header.php
+++ wp-content/themes/understrap-master/header.php
@@ -28,41 +28,39 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<!-- ******************* The Navbar Area ******************* -->
 	<div id="wrapper-navbar">
 
-		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
-
-		<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-labelledby="main-nav-label">
-
-			<h2 id="main-nav-label" class="sr-only">
-				<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
-			</h2>
-
-		<?php if ( 'container' === $container ) : ?>
+		<div class="top-header">
+		
 			<div class="container">
-		<?php endif; ?>
+			
+				<div class="row">
+				
+					<div class="col-lg5">
+					
+						<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo">
 
-					<!-- Your site title as branding in the menu -->
-					<?php if ( ! has_custom_logo() ) { ?>
+					</div>
 
-						<?php if ( is_front_page() && is_home() ) : ?>
+					<div class="col-lg7">
+					
+					
+					</div>
 
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+				</div>
 
-						<?php else : ?>
+			</div>
 
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+		</div>
 
-						<?php endif; ?>
+		<nav id="main-nav" class="navbar navbar-expand-lg navbar-dark bg-primary" aria-labelledby="main-nav-label">
 
-						<?php
-					} else {
-						the_custom_logo();
-					}
-					?>
-					<!-- end custom logo -->
 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+			<div class="container">
+
+
+
+				<!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
 					<span class="navbar-toggler-icon"></span>
-				</button>
+				</button> -->
 
 				<!-- The WordPress Menu goes here -->
 				<?php
@@ -79,9 +77,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 					)
 				);
 				?>
-			<?php if ( 'container' === $container ) : ?>
+
 			</div><!-- .container -->
-			<?php endif; ?>
+
 
 		</nav><!-- .site-navigation -->
 
